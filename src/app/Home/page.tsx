796a72832bf590004201cd9ff7d9bdc8ab0f1313
Sergej <sakacszergej@gmail.com>
2025-02-19 17:34:12 +0100

    first commit

---
+++ src/app/Home/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./home.module.scss";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.title}>
+        Coretime Hub V2
+      </h1>
+    </div>
+  );
+}
