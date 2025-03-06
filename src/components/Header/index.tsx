15ea023ad0b48bdc7c6c9432cddb5f6166e9947d
Petar <petargrahovac23@gmail.com>
2025-03-06 15:23:42 +0100

    Coretime menu &  pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,23 @@
 import { useState } from "react";
 import { useUnit } from "effector-react";
 import Image from "next/image";
+import { useRouter } from "next/router";
 import styles from "./header.module.scss";
 import AccountSelector from "@/components/AccountSelector";
 import NetworkSelector from "@/components/NetworkSelector";
 import WalletModal from "../WalletModal/WalletModal";
+import CoretimeMenu from "../CoretimeMenu/index";
 import { Button } from "@region-x/components";
 import { $loadedAccounts } from "@/wallet";
 
-const Header = () => {
+const Header: React.FC = () => {
   const accounts = useUnit($loadedAccounts);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  const handleNavigation = (path: string) => {
+    router.push(`/${path}`);
+  };
 
   return (
     <nav className={styles.navbar}>
@@ -23,13 +30,31 @@ const Header = () => {
       />
       <div className={styles.list}>
         <ul className={styles.navList}>
-          <li className={styles.navItem}>Home</li>
-          <li className={styles.navItem}>Coretime</li>
-          <li className={styles.navItem}>Cross-Chain</li>
-          <li className={styles.navItem}>Parachain Dashboard</li>
-          <li className={styles.navItem}>Secondary Market</li>
+          <li className={styles.navItem} onClick={() => handleNavigation("")}>
+            Home
+          </li>
+          <CoretimeMenu />
+          <li
+            className={styles.navItem}
+            onClick={() => handleNavigation("cross-chain")}
+          >
+            Cross-Chain
+          </li>
+          <li
+            className={styles.navItem}
+            onClick={() => handleNavigation("parachain-dashboard")}
+          >
+            Parachain Dashboard
+          </li>
+          <li
+            className={styles.navItem}
+            onClick={() => handleNavigation("secondary-market")}
+          >
+            Secondary Market
+          </li>
         </ul>
       </div>
+
       <div className={styles.content}>
         {accounts.length > 0 ? (
           <>
---
+++ src/pages/coretime/purchase.tsx
@@ -0,0 +1,10 @@
+const PurchasePage = () => {
+  return (
+    <div>
+      <h1>Purchase Page</h1>
+      <p>This is the Purchase page.</p>
+    </div>
+  );
+};
+
+export default PurchasePage;
