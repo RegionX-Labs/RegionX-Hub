73037d6463290d4f9cc7ccb81c565519006ac9c7
Sergej <sakacszergej@gmail.com>
2025-03-07 13:57:26 +0100

    Fix loading extensions

--- src/pages/Home/page.tsx
+++ src/pages/Home/page.tsx
@@ -1,46 +1,7 @@
 import styles from './home.module.scss';
-import { networkStarted } from '@/api/connection';
-import { useEffect } from 'react';
-import { Network } from '@/types';
-import { useRouter } from 'next/router';
-import { getExtensions } from '@/wallet';
-import { Button } from '@region-x/components';
 import Header from '@/components/Header';
-import { burnInfoRequested } from '@/coretime/burnInfo';
-import { purchaseHistoryRequested } from '@/coretime/purchaseHistory';
 
 export default function Home() {
-  const router = useRouter();
-  const { network } = router.query;
-
-  useEffect(() => {
-    let _network = Network.NONE;
-    if (!router.isReady) return;
-    if (network === 'polkadot') _network = Network.POLKADOT;
-    else if (network === 'kusama') _network = Network.KUSAMA;
-    else if (network === 'paseo') _network = Network.PASEO;
-    else if (network === 'rococo') _network = Network.ROCOCO;
-    else if (network === 'westend') _network = Network.WESTEND;
-    else {
-      // invalid network param. redirect to the default chain: polkadot
-      router.push(
-        {
-          pathname: router.pathname,
-          query: {
-            ...router.query,
-            network: 'polkadot',
-          },
-        },
-        undefined,
-        { shallow: false }
-      );
-    }
-    networkStarted(_network);
-    getExtensions();
-    burnInfoRequested(_network);
-    purchaseHistoryRequested({ network: _network, saleCycle: 1 });
-  }, [network, router, router.isReady]);
-
   return (
     <div className={styles.page}>
       <Header />
