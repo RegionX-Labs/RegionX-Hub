da9a1ab32b0661b86263db528feeb3e44d592e07
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-03-13 08:39:09 +0100

    my regions (#22)
    * my regions
    
    * my regions page
    
    * routing & radius fix
    
    * small fix
    
    * select region
    
    * region fix
    
    * region card & routing fix
    
    * Update src/pages/coretime/MyRegions/index.tsx
    
    * Update src/pages/coretime/MyRegions/index.tsx
    
    * Update src/pages/coretime/MyRegions/index.tsx
    
    ---------
    
    Co-authored-by: Sergej Sakac <73715684+Szegoo@users.noreply.github.com>

--- src/components/CoretimeMenu/index.tsx
+++ src/components/CoretimeMenu/index.tsx
@@ -36,7 +36,7 @@ const CoretimeMenu: React.FC = () => {
           onMouseEnter={handleMouseEnter}
           onMouseLeave={handleMouseLeave}
         >
-          <li className={styles.subMenuItem} onClick={() => handleNavigation('my-regions')}>
+          <li className={styles.subMenuItem} onClick={() => handleNavigation('MyRegions')}>
             My Regions
           </li>
           <li className={styles.subMenuItem} onClick={() => handleNavigation('renew')}>
---
+++ src/pages/coretime/MyRegions/index.tsx
@@ -0,0 +1,92 @@
+import { $regions, regionsRequested } from '@/coretime/regions';
+import { useUnit } from 'effector-react';
+import { $network } from '@/api/connection';
+import { RegionCard } from '@region-x/components';
+import { useEffect, useState } from 'react';
+import styles from './my-regions.module.scss';
+
+const MyRegionsPage = () => {
+  const network = useUnit($network);
+  const regions = useUnit($regions);
+  const [selectedRegionId, setSelectedRegionId] = useState<string | null>(null);
+
+  const countBits = (regionMask: string) => {
+    let count = 0;
+    // Convert hex to bits and count ones.
+    for (let i = 2; i < regionMask.length; ++i) {
+      let v = parseInt(regionMask.slice(i, i + 1), 16);
+      while (v > 0) {
+        if (v & 1) ++count;
+        v >>= 1;
+      }
+    }
+
+    return count;
+  };
+
+  useEffect(() => {
+    regionsRequested(network);
+  }, [network]);
+
+  useEffect(() => {
+    console.log(regions);
+  }, [regions]);
+
+  return (
+    <>
+      <div className={styles.container}>
+        {regions.length > 0 ? (
+          regions.map((region) => (
+            <div className={styles['region-card']} key={region.id}>
+              {' '}
+              <RegionCard
+                selected={selectedRegionId == region.id}
+                regionData={{
+                  chainColor: 'greenDark',
+                  chainLabel: 'Coretime Chain',
+                  coreIndex: region.core,
+                  consumed: 0,
+                  // 57600 / 80 = 720
+                  coreOcupaccy: ((countBits(region.mask) * 720) / 57600) * 100,
+                  duration: '28 days', // TODO,
+                  name: '', // TODO
+                  regionEnd: region.end.toString(), // TODO: Human readable format
+                  regionStart: region.begin.toString(), // TODO: Human readable format
+                  currentUsage: 0, // TODO
+                  onClick: () => setSelectedRegionId(region.id),
+                }}
+                task={`Unassigned`} // TODO
+              />
+            </div>
+          ))
+        ) : (
+          <p>No regions available.</p>
+        )}
+      </div>
+      <div>
+        {' '}
+        <nav className={styles.menu}>
+          <ul>
+            <li>
+              <a href='#partition'>Partition</a>
+            </li>
+            <li>
+              <a href='#interface'>Interface</a>
+            </li>
+            <li>
+              <a href='#transfer'>Transfer</a>
+            </li>
+            <li>
+              <a href='#assign'>Assign</a>
+            </li>
+            <li>
+              <a href='#sell'>Sell</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
+  );
+};
+
+export default MyRegionsPage;
