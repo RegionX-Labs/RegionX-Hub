9ed8e16a5e4ea5b45f0c9aa3b6130f270de73ecd
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-04-23 11:24:08 +0200

    Current auction price (#51)
    * auction phase integration
    
    * current auction price integration
    
    * current price data integration
    
    * components wrap
    
    * data for timer

--- src/components/Home/HomeDashboard/index.tsx
+++ src/components/Home/HomeDashboard/index.tsx
@@ -1,6 +1,7 @@
 import MyCore from './MyCore';
 import CoreComparison from './CoreComparison';
 import DutchAuctionChart from './DutchAuctionChart';
+import AuctionPhaseStatus from './AuctionPhaseStatus';
 import styles from './HomeDashboard.module.scss';
 
 export default function HomeDashboard() {
@@ -8,6 +9,7 @@ export default function HomeDashboard() {
     <div className={styles.dashboard}>
       <MyCore />
       <CoreComparison />
+      <AuctionPhaseStatus />
       <DutchAuctionChart />
     </div>
   );
