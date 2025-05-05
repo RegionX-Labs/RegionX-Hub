11e9943eb922359c1ae338d203495ebeaf5575ef
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-05-05 20:17:02 +0200

    Purchase History Table (#59)
    * first commit
    
    * small update
    
    * table width

--- src/components/Home/HomeDashboard/index.tsx
+++ src/components/Home/HomeDashboard/index.tsx
@@ -1,18 +1,42 @@
+import { useEffect, useRef, useState } from 'react';
 import RenewableCores from './RenewableCores';
 import CoreComparison from './CoreComparison';
 import DutchAuctionChart from './DutchAuctionChart';
 import AuctionPhaseStatus from './AuctionPhaseStatus';
 import CorePurchaseCard from './CorePurchaseCard';
+import PurchaseHistoryTable from './PurchaseHistoryTable';
 import styles from './HomeDashboard.module.scss';
 
 export default function HomeDashboard() {
+  const wrapperRef = useRef<HTMLDivElement>(null);
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const wrapper = wrapperRef.current;
+    if (!wrapper) return;
+
+    const handleScroll = () => {
+      setScrolled(wrapper.scrollTop > 0);
+    };
+
+    wrapper.addEventListener('scroll', handleScroll);
+    return () => wrapper.removeEventListener('scroll', handleScroll);
+  }, []);
+
   return (
-    <div className={styles.dashboard}>
-      <RenewableCores />
-      <CoreComparison />
-      <AuctionPhaseStatus />
-      <DutchAuctionChart />
-      <CorePurchaseCard />
+    <div
+      className={`${styles.dashboardWrapper} ${scrolled ? styles.scrolled : ''}`}
+      ref={wrapperRef}
+    >
+      <div className={styles.dashboard}>
+        <RenewableCores />
+        <CoreComparison />
+        <CorePurchaseCard />
+
+        <AuctionPhaseStatus />
+        <DutchAuctionChart />
+        <PurchaseHistoryTable />
+      </div>
     </div>
   );
 }
