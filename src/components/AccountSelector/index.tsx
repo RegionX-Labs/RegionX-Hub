dc7bf03b23c403df1b7d4700d8edbbc7e777e5d3
Sergej <sakacszergej@gmail.com>
2025-05-08 12:32:51 +0200

    fix disconnect

--- src/components/AccountSelector/index.tsx
+++ src/components/AccountSelector/index.tsx
@@ -1,4 +1,4 @@
-import { $loadedAccounts, accountSelected, walletSelected } from '@/wallet';
+import { $loadedAccounts, accountSelected, SELECTED_WALLET_KEY, walletSelected } from '@/wallet';
 import { useUnit } from 'effector-react';
 import Identicon from '@polkadot/react-identicon';
 import styles from './account.module.scss';
@@ -11,6 +11,7 @@ const AccountSelector = () => {
     if (value === 'disconnect') {
       console.log('disconnect');
       walletSelected('');
+      localStorage.removeItem(SELECTED_WALLET_KEY);
       return;
     }
     if (value) {
