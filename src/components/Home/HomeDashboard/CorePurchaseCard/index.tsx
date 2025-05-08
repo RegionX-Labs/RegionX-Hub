9a520bcfc7bdedb4b90c53d811b1f9a927544e3d
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2025-05-08 10:58:36 +0200

    buy core implementation (#63)
    * buy core implementation
    
    * sign and submit
    
    * check for interlude and cores offered
    
    * prettier

--- src/components/Home/HomeDashboard/CorePurchaseCard/index.tsx
+++ src/components/Home/HomeDashboard/CorePurchaseCard/index.tsx
@@ -1,22 +1,31 @@
 import { useEffect, useState } from 'react';
 import { useUnit } from 'effector-react';
-import { $latestSaleInfo, latestSaleRequested } from '@/coretime/saleInfo';
+import {
+  $latestSaleInfo,
+  getCurrentPhase,
+  latestSaleRequested,
+  SalePhase,
+} from '@/coretime/saleInfo';
 import { $purchaseHistory, purchaseHistoryRequested } from '@/coretime/purchaseHistory';
 import { $connections, $network } from '@/api/connection';
 import { getCorePriceAt, toUnitFormatted } from '@/utils';
 import styles from './CorePurchaseCard.module.scss';
 import { getNetworkChainIds, getNetworkMetadata } from '@/network';
+import toast, { Toaster } from 'react-hot-toast';
+import { $selectedAccount } from '@/wallet';
 
 export default function CorePurchaseCard() {
-  const [connections, network, saleInfo, purchaseHistory] = useUnit([
+  const [connections, network, saleInfo, purchaseHistory, selectedAccount] = useUnit([
     $connections,
     $network,
     $latestSaleInfo,
     $purchaseHistory,
+    $selectedAccount,
   ]);
 
   const [corePrice, setCorePrice] = useState<number | null>(null);
   const [currentHeight, setCurrentHeight] = useState<number>(0);
+  const [currentPhase, setCurrentPhase] = useState<SalePhase | null>(null);
 
   useEffect(() => {
     if (network) latestSaleRequested(network);
@@ -49,9 +58,78 @@ export default function CorePurchaseCard() {
     }
   }, [saleInfo?.network, connections]);
 
+  useEffect(() => {
+    (async () => {
+      if (!saleInfo) return;
+      const networkChainIds = getNetworkChainIds(network);
+      if (!networkChainIds) return;
+      const connection = connections[networkChainIds.coretimeChain];
+      if (!connection || !connection.client || connection.status !== 'connected') return;
+
+      const client = connection.client;
+      const metadata = getNetworkMetadata(network);
+      if (!metadata) return;
+
+      const currentBlockNumber = await (
+        client.getTypedApi(metadata.coretimeChain) as any
+      ).query.System.Number.getValue();
+      const phase = getCurrentPhase(saleInfo, currentBlockNumber);
+      setCurrentPhase(phase);
+    })();
+  }, [network, saleInfo]);
+
   const coresSold = purchaseHistory.length;
   const coresOffered = saleInfo?.coresOffered ?? 0;
   const coresRemaining = coresOffered - coresSold;
+
+  const buyCore = async () => {
+    if (!selectedAccount) {
+      toast.error('Account not selected');
+      return;
+    }
+    if (currentPhase === SalePhase.Interlude) {
+      toast.error('Cannot purchase a core during interlude phase');
+      return;
+    }
+    if (saleInfo?.coresSold == saleInfo?.coresOffered) {
+      toast.error('No more cores remaining');
+      return;
+    }
+
+    const networkChainIds = getNetworkChainIds(network);
+    if (!networkChainIds) {
+      toast.error('Unknown network');
+      return;
+    }
+    const connection = connections[networkChainIds.coretimeChain];
+    if (!connection || !connection.client || connection.status !== 'connected') {
+      toast.error('Failed to connect to the API');
+      return;
+    }
+
+    const client = connection.client;
+    const metadata = getNetworkMetadata(network);
+    if (!metadata) {
+      toast.error('Failed to find metadata of the chains');
+      return;
+    }
+
+    if (!corePrice) {
+      toast.error('Failed to fetch the price of a core');
+      return;
+    }
+
+    const tx = (client.getTypedApi(metadata.coretimeChain).tx as any).Broker.purchase({
+      price_limit: BigInt(corePrice),
+    });
+    const res = await tx.signAndSubmit(selectedAccount.polkadotSigner);
+    if (res.ok) {
+      toast.success('Transaction succeded!');
+    } else {
+      // TODO: provide more detailed error
+      toast.error('Transaction failed');
+    }
+  };
 
   return (
     <div className={styles.coreRemainingCard}>
@@ -69,7 +147,10 @@ export default function CorePurchaseCard() {
         </span>
       </div>
 
-      <button className={styles.buyButton}>Buy Core</button>
+      <button onClick={buyCore} className={styles.buyButton}>
+        Buy Core
+      </button>
+      <Toaster />
     </div>
   );
 }
