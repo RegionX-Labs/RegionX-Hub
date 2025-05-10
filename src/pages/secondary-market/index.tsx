3795567fcaa9cfae1c5204b367644a36b3e83f06
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-05-10 12:30:52 +0200

    Bug fixes (#69)
    * Auction Phase Status responsive fix
    
    * rpc button position fix
    
    * update
    
    * network select fix
    
    * secondary-market page
    
    * update
    
    * small update
    
    * final fix

---
+++ src/pages/secondary-market/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './secondary-market.module.scss';
+
+const SecondaryMarket = () => {
+  return (
+    <div className={styles.secondaryMarketContainer}>
+      <h1 className={styles.comingSoonLabel}>Coming Soon!</h1>
+    </div>
+  );
+};
+
+export default SecondaryMarket;
