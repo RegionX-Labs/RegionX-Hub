6c2851daae01434dc0f6ea3a74421cd2220ca5b9
Sergej <sakacszergej@gmail.com>
2025-05-20 17:20:56 +0200

    Fix font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,6 +5,12 @@ export default function HomePage() {
   return (
     <>
       <Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap"
+          rel="stylesheet"
+        />
         <title>RegionX Hub</title>
       </Head>
       <Home />
