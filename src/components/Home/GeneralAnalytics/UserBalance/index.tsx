e0712b8b3a7ae7f1a7c9cb8482833526899f3624
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-05-21 10:11:33 +0200

    user balance integration (#85)
    * user balance integration
    
    * fix
    
    * text update
    
    ---------
    
    Co-authored-by: Sergej <sakacszergej@gmail.com>

---
+++ src/components/Home/GeneralAnalytics/UserBalance/index.tsx
@@ -0,0 +1,47 @@
+import { useUnit } from 'effector-react';
+import styles from './UserBalance.module.scss';
+import { $network } from '@/api/connection';
+import { $selectedAccount } from '@/wallet';
+import { $accountData } from '@/account';
+import { toUnitFormatted } from '@/utils';
+import { useRouter } from 'next/router';
+
+export default function UserBalance() {
+  const router = useRouter();
+
+  const [network, selectedAccount, accountDataMap] = useUnit([
+    $network,
+    $selectedAccount,
+    $accountData,
+  ]);
+
+  if (!selectedAccount || !accountDataMap) return null;
+
+  const accountData = accountDataMap[selectedAccount.address];
+  if (!accountData || !accountData.coretimeChainData) return null;
+
+  const formatted = toUnitFormatted(network, accountData.coretimeChainData.free);
+
+  const handleTransferClick = () => {
+    router.push('/cross-chain');
+  };
+
+  return (
+    <div className={styles.metricBox}>
+      <span className={styles.metricLabel}>Coretime Chain Balance</span>
+      <h3 className={styles.coretimeValue}>{formatted}</h3>
+
+      <p className={styles.infoText}>
+        This balance is used to <strong>purchase</strong> or <strong>renew</strong> a core
+      </p>
+      <p className={styles.note}>
+        To fund the Coretime chain balance, you must transfer tokens <br />
+        <strong>cross-chain</strong> from the Relay Chain to the Coretime Chain.
+      </p>
+
+      <button className={styles.transferButton} onClick={handleTransferClick}>
+        Transfer
+      </button>
+    </div>
+  );
+}
