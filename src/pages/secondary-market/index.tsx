9427177ad05d24db1a765d1b43d2c35d01f69dfc
Petar Grahovac <123510382+Grahovacc@users.noreply.github.com>
2025-05-27 19:43:44 +0200

    secondary market overview integration (#112)
    * secondary market overview integration
    
    * user balance integration
    
    * small fixes & prettier
    
    ---------
    
    Co-authored-by: Sergej <sakacszergej@gmail.com>

--- src/pages/secondary-market/index.tsx
+++ src/pages/secondary-market/index.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styles from './secondary-market.module.scss';
 import SecondaryMarketplaceTable from '../../components/SecondaryMarketplaceTable';
 import AuctionPriceOverview from '@/components/Home/GeneralAnalytics/AuctionPriceOverview';
+import SecondaryMarketOverview from '@/components/SecondaryMarketOverview';
+import { useUnit } from 'effector-react';
+import { $network } from '@/api/connection';
+import { latestSaleRequested } from '@/coretime/saleInfo';
 
 export default function SecondaryMarket() {
+  const network = useUnit($network);
+
+  useEffect(() => {
+    if (network) latestSaleRequested(network);
+  }, [network]);
+
   return (
     <div className={styles.secondaryMarketPage}>
-      <div className={styles.AuctionPriceOverview}>
-        {' '}
-        <AuctionPriceOverview />
+      <div className={styles.cardsRow}>
+        <div className={styles.SecondaryMarketOverview}>
+          <SecondaryMarketOverview />
+        </div>
+        <div className={styles.AuctionPriceOverview}>
+          <AuctionPriceOverview />
+        </div>
+        <div className={styles.PricingChartCard}>
+          <AuctionPriceOverview />
+        </div>
       </div>
       <SecondaryMarketplaceTable />
     </div>
