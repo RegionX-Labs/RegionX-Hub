3d51b9b9d9373ad462c3887bcf63e76514049e55
Sergej Sakac <73715684+Szegoo@users.noreply.github.com>
2025-07-16 12:57:05 +0200

    Fix pricing (#178)

--- src/components/Home/HomeDashboard/DutchAuctionChart/index.tsx
+++ src/components/Home/HomeDashboard/DutchAuctionChart/index.tsx
@@ -38,58 +38,50 @@ export default function DutchAuctionChart({ theme, view }: DutchAuctionChartProp
     })();
   }, [network, connections]);
 
-  // Returns min price if price is below it.
-  const priceOrMin = (price: bigint): bigint => {
-    const minPrice = getMinEndPrice(network);
-    return price < minPrice ? minPrice : price;
-  };
-
   const data = [
     {
       timestamp: phaseEndpoints?.interlude.start,
-      value: toUnit(network, priceOrMin(renewalPrice)),
+      value: toUnit(network, renewalPrice),
       phase: SalePhase.Interlude,
     },
     {
       timestamp: phaseEndpoints?.interlude.end,
-      value: toUnit(network, priceOrMin(renewalPrice)),
+      value: toUnit(network, renewalPrice),
       phase: SalePhase.Interlude,
     },
     {
       timestamp: phaseEndpoints?.interlude.end,
-      value: toUnit(network, priceOrMin(renewalPrice)),
+      value: toUnit(network, renewalPrice),
       phase: SalePhase.Leadin,
     },
     {
       timestamp: phaseEndpoints?.leadin.start,
       value: toUnit(
         network,
-        priceOrMin(
-          phaseEndpoints && saleInfo
-            ? BigInt(getCorePriceAt(saleInfo.saleStart, saleInfo, network))
-            : BigInt(0)
-        )
+        phaseEndpoints && saleInfo
+          ? BigInt(getCorePriceAt(saleInfo.saleStart, saleInfo, network))
+          : BigInt(0)
       ),
       phase: SalePhase.Leadin,
     },
     {
       timestamp: phaseEndpoints && (phaseEndpoints.leadin.start + phaseEndpoints.leadin.end) / 2,
-      value: toUnit(network, priceOrMin(BigInt(saleInfo?.endPrice || '0') * BigInt(10))),
+      value: toUnit(network, BigInt(saleInfo?.endPrice || '0') * BigInt(10)),
       phase: SalePhase.Leadin,
     },
     {
       timestamp: phaseEndpoints?.leadin.end,
-      value: toUnit(network, priceOrMin(BigInt(saleInfo?.endPrice || '0'))),
+      value: toUnit(network, BigInt(saleInfo?.endPrice || '0')),
       phase: SalePhase.Leadin,
     },
     {
       timestamp: phaseEndpoints?.fixed.start,
-      value: toUnit(network, priceOrMin(BigInt(saleInfo?.endPrice || '0'))),
+      value: toUnit(network, BigInt(saleInfo?.endPrice || '0')),
       phase: SalePhase.FixedPrice,
     },
     {
       timestamp: phaseEndpoints?.fixed.end,
-      value: toUnit(network, priceOrMin(BigInt(saleInfo?.endPrice || '0'))),
+      value: toUnit(network, BigInt(saleInfo?.endPrice || '0')),
       phase: SalePhase.FixedPrice,
     },
   ];
